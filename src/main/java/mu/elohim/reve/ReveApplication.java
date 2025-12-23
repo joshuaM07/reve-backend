1def6c729cd5f254a84a4b07bc5019f346d41967
joshuaM07 <43468436+joshuaM07@users.noreply.github.com>
2025-12-23 17:45:50 +0400

    Init

---
+++ src/main/java/mu/elohim/reve/ReveApplication.java
@@ -0,0 +1,13 @@
+package mu.elohim.reve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mu/elohim/reve/ReveApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.elohim.reve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
